5dfca444cc8b70166de49ebe68984008aab5b73e
Timur <emailtotimur@gmail.com>
2017-09-10 23:46:31 +0300

    first version

---
+++ Compressor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Compressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelRoute = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.labelFolderForSave = new System.Windows.Forms.Label();
+            this.labelSaved = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // labelRoute
+            // 
+            this.labelRoute.AutoSize = true;
+            this.labelRoute.Location = new System.Drawing.Point(13, 74);
+            this.labelRoute.Name = "labelRoute";
+            this.labelRoute.Size = new System.Drawing.Size(0, 13);
+            this.labelRoute.TabIndex = 2;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(16, 36);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(256, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Открыть папку с изображениями";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // labelFolderForSave
+            // 
+            this.labelFolderForSave.AutoSize = true;
+            this.labelFolderForSave.Location = new System.Drawing.Point(13, 166);
+            this.labelFolderForSave.Name = "labelFolderForSave";
+            this.labelFolderForSave.Size = new System.Drawing.Size(0, 13);
+            this.labelFolderForSave.TabIndex = 4;
+            // 
+            // labelSaved
+            // 
+            this.labelSaved.AutoSize = true;
+            this.labelSaved.Location = new System.Drawing.Point(12, 144);
+            this.labelSaved.Name = "labelSaved";
+            this.labelSaved.Size = new System.Drawing.Size(239, 13);
+            this.labelSaved.TabIndex = 5;
+            this.labelSaved.Text = "Изображения сохранены в папку compressed:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(16, 104);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(256, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Запустить сжатие изображений";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(19, 210);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(253, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.labelSaved);
+            this.Controls.Add(this.labelFolderForSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.labelRoute);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Compressor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label labelRoute;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label labelFolderForSave;
+        private System.Windows.Forms.Label labelSaved;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar;
+
+    }
+}
+
---
+++ Compressor/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compressor
+{
+    public partial class Form1 : Form
+    {
+        string saveRoute;
+        string filesPath;
+
+        Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+            btnStart.Enabled = false;
+            labelSaved.Visible = false;
+            progressBar.Visible = false;
+        }
+
+        public void ChangeImage()
+        {
+            var files = Directory.GetFiles(folderBrowserDialog.SelectedPath, "*jpg*", SearchOption.AllDirectories);
+
+            progressBar.Invoke((MethodInvoker)delegate // Running on the UI
+            {
+                progressBar.Visible = true;
+                progressBar.Maximum = files.Length;
+            });
+
+            foreach (var path in files)
+            {
+                string fileName = System.IO.Path.GetFileName(path.ToString()); //name of file
+                string fullRoute = filesPath + "\\copressed\\" + fileName; // full way to file
+
+                progressBar.Invoke((MethodInvoker)delegate
+                {
+                    progressBar.Value++;
+                });
+                try
+                {
+                    if (!Directory.Exists(saveRoute))
+                    {
+                        Directory.CreateDirectory(saveRoute); // If the directory doesn't exist, create it.
+                    }
+                }
+                catch
+                {
+
+                }
+
+                SqueezeImage(path.ToString(), fullRoute);
+            }
+            MessageBox.Show("Изображения сохранены в " + saveRoute);
+            progressBar.Invoke((MethodInvoker)delegate
+            {
+                progressBar.Value = 0;
+                labelSaved.Visible = true;
+                labelFolderForSave.Text = saveRoute;
+            });
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                filesPath = folderBrowserDialog.SelectedPath;
+                labelRoute.Text = filesPath;
+                saveRoute = filesPath + "\\copressed"; //way to folder where files will be saved
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(ChangeImage);
+            thread.Start();
+        }
+
+        private void SqueezeImage(string filePath, string route)
+        {
+            Bitmap bmp1 = new Bitmap(filePath);
+            // ScaleImage(bmp1, 700, 700);
+            ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+            // Create an Encoder object based on the GUID
+            // for the Quality parameter category.
+            System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+            // Create an EncoderParameters object.
+            // An EncoderParameters object has an array of EncoderParameter
+            // objects. In this case, there is only one
+            // EncoderParameter object in the array.
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 70L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            //using (var newImage = ResizeImage(bmp1, 700, 400))
+            //{
+            //    newImage.Save(route, jpgEncoder, myEncoderParameters);
+            //}
+
+            bmp1.Save(route, jpgEncoder, myEncoderParameters);
+            bmp1.Dispose();
+
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        public static Image ResizeImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Max(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+    }
+}
